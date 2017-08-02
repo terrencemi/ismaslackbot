9c1729cf44afd78229998c97a8f3584aa06b130e
Isma Terrence <ismaterrence@Ismas-MacBook-Pro.local>
2017-08-02 00:15:10 -0400

    Hello World

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
-  res.send('hello world’);
-});
+  res.send('Hello World!')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port: ' + port)
+})
